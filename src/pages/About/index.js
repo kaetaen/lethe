d2c55f3f5fe86e2959d5ce118887cc6a01d1275c
kaetaen <kaetaen@tutanota.com>
2022-01-02 18:02:00 -0300

    Add about me

---
+++ src/pages/About/index.js
@@ -0,0 +1,11 @@
+function About () {
+  return (
+    <div>
+      <h1> About Me </h1>
+      <p>Hi, my name is Rubens. Find me on <a href="https://github.com/kaetaen" target="_blank">Github</a></p>
+    </div>
+  )
+}
+
+
+export default About
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,5 +4,7 @@ function Dashboard () {
       <h1> Welcome to the Lethe </h1>
       <p>A React boilerplate, based on Create React App</p>
     </div>
-  )
+ )
 }
+
+export default Dashboard
