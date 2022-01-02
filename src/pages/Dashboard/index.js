7c093758e38fef3e381e681bcd97be1e2bf2bf28
kaetaen <kaetaen@tutanota.com>
2022-01-02 18:31:30 -0300

    Add redirect to /about

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Dashboard () {
   return (
     <div>
       <h1> Welcome to the Lethe </h1>
       <p>A React boilerplate, based on Create React App</p>
+      <Link to="/About"> About me </Link>
     </div>
  )
 }
